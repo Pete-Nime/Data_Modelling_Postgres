23fa8549dbca9a4dc4381af03bb4ac0b34664c97
Pete-Nime <nimepeter77@gmail.com>
2022-12-16 14:53:08 +1300

    READ ME, sql, test file
    Updating read me, sql_queries, & test files

--- sql_queries.py
+++ sql_queries.py
@@ -6,7 +6,7 @@ song_table_drop = "DROP TABLE IF EXISTS songs"
 artist_table_drop = "DROP TABLE IF EXISTS artists"
 time_table_drop = "DROP TABLE IF EXISTS time"
 
-# CREATE TABLES
+# CREATE TABLES (Facts table)
 
 songplay_table_create = ("""
 CREATE TABLE IF NOT EXISTS songplays (
@@ -22,12 +22,14 @@ CREATE TABLE IF NOT EXISTS songplays (
 );
 """)
 
+# Dimensional tables
+
 user_table_create = ("""
 CREATE TABLE IF NOT EXISTS users (
     user_id int PRIMARY KEY,
-    first_name varchar,
-    last_name varchar,
-    gender varchar,
+    first_name varchar NOT NULL,
+    last_name varchar NOT NULL,
+    gender varchar NOT NULL,
     level varchar
 );
 """)
@@ -36,35 +38,35 @@ song_table_create = ("""
 CREATE TABLE IF NOT EXISTS songs (
     song_id varchar PRIMARY KEY,
     title varchar,
-    artist_id varchar NOT NULL,
-    year int,
-    duration numeric
+    artist_id varchar,
+    year int NOT NULL,
+    duration float
 );
 """)
 
 artist_table_create = ("""
 CREATE TABLE IF NOT EXISTS artists (
-    artist_id varchar PRIMARY KEY,
-    name varchar,
-    location varchar,
-    latitude numeric,
-    longitude numeric
+    artist_id varchar PRIMARY KEY NOT NULL,
+    name varchar NOT NULL,
+    location varchar NOT NULL,
+    latitude float,
+    longitude float
 );
 """)
 
 time_table_create = ("""
 CREATE TABLE IF NOT EXISTS time (
     start_time timestamp PRIMARY KEY,
-    hour int,
+    hour int NOT NULL,
     day int,
     week int,
     month int,
-    year int,
-    weekday int
+    year int NOT NULL,
+    weekday VARCHAR
 );
 """)
 
-# INSERT RECORDS
+# INSERTING THE  RECORDS
 
 songplay_table_insert = ("""
 INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
@@ -103,7 +105,7 @@ ON CONFLICT (start_time)
 DO NOTHING
 """)
 
-# FIND SONGS
+# FINDING THE SONGS
 
 song_select = ("""
 SELECT song_id, songs.artist_id FROM songs 
@@ -113,7 +115,7 @@ AND name = %s
 AND duration = %s;
 """)
 
-# QUERY LISTS
+# QUERY ON LISTS
 
 create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
